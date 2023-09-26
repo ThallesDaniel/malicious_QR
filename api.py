261c4476466aa071a1ef0b018dfc5660b1d400c2
Thalles Daniel <106764514+ThallesDaniel@users.noreply.github.com>
2023-09-26 10:47:30 -0300

    Update api.py
    Retirada de comentários e trechos de códigos desnecessários

--- api.py
+++ api.py
@@ -25,32 +25,27 @@ def capturar_informacoes():
 def enviar_email(ip, nome_aparelho):
     try:
         # Configurar a conexão SMTP
-        smtp_server = 'smtp.gmail.com'  # Substitua pelo servidor SMTP do seu provedor
-        smtp_port = 587  # Porta SMTP do seu provedor (geralmente 587 ou 465)
-        smtp_username = 'teste@teste.com'  # Seu endereço de e-mail institucional
+        smtp_server = 'smtp.gmail.com' 
+        smtp_port = 587  
+        smtp_username = 'teste@teste.com'  # Seu endereço de e-mail 
         smtp_password = 'UVB76Ckd3301'  # Sua senha de e-mail
 
         destinatario = 'teste@gmail.com'  # Substitua pelo endereço de e-mail do destinatário
 
-        # Configurar a conexão SMTP
         server = smtplib.SMTP(smtp_server, smtp_port)
         server.starttls()
         server.login(smtp_username, smtp_password)
 
-        # Criar uma mensagem de e-mail
         msg = MIMEMultipart()
         msg['From'] = smtp_username
         msg['To'] = destinatario
         msg['Subject'] = 'Informações do QR Code Lido'
-
-        # Corpo do e-mail
         corpo_email = f"IP: {ip}\nNome do Aparelho: {nome_aparelho}"
         msg.attach(MIMEText(corpo_email, 'plain'))
 
-        # Enviar o e-mail
         server.sendmail(smtp_username, destinatario, msg.as_string())
 
-        # Encerrar a conexão SMTP
+
         server.quit()
 
         return "E-mail enviado com sucesso!"
